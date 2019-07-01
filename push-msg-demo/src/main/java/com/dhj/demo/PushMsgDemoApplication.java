5246945850e817953bea768af00c1b78e7adce76
邓海京 <2505728250@qq.com>
2019-07-01 18:29:25 +0800

    frist commit

---
+++ push-msg-demo/src/main/java/com/dhj/demo/PushMsgDemoApplication.java
@@ -0,0 +1,13 @@
+package com.dhj.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PushMsgDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PushMsgDemoApplication.class, args);
+    }
+
+}
---
+++ push-msg-demo/src/test/java/com/dhj/demo/PushMsgDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dhj.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PushMsgDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
